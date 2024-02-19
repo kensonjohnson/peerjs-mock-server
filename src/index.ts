77f6709434ab9cdc72638e669acef5d7fd0ca51f
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-02-18 20:22:17 -0600

    Create simple peerjs server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const myMessage: String = "Hello TypeScript!";
+import { PeerServer } from "peer";
 
-console.log(myMessage);
+const PORT = 9002;
+
+const peerServer = PeerServer({
+  port: PORT,
+  key: "demodemo",
+  path: "/myapp",
+  allow_discovery: true,
+});
+
+peerServer.on("connection", (client) => {
+  console.log("Connection ID:", client.getId());
+});
+
+peerServer.on("disconnect", (client) => {
+  console.log("Disconnect ID:", client.getId());
+});
+
+console.log(`PeerJS Server is running on port ${PORT}`);
